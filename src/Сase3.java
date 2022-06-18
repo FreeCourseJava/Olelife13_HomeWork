b759d699c8b147ff0c38ca62dc1ac595f6c777e1
Olelife13 <olelife13@gmail.com>
2022-06-19 02:13:54 +0700

    Задание 3

---
+++ src/Сase3.java
@@ -0,0 +1,7 @@
+public class Сase3 {
+    public static void main(String[] args) {
+        // Результат равен: значение_переменной овец (штук)
+        int otara = 888;
+        System.out.print("Результат равен: "+otara+" овец (штук).");
+    }
+}
