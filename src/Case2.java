7f7fc623486fdf56ae53619aed6c63e46756d759
Olelife13 <olelife13@gmail.com>
2022-06-19 01:46:20 +0700

    Задание 2

---
+++ src/Case2.java
@@ -0,0 +1,12 @@
+public class Case2 {
+    // ПервоначальнояЯ видела только грустную надпись "java: incompatible types: possible lossy conversion
+    // from int to char" и грустила про несовместимость всего живого в этом мире. После второго урока немного
+    // попробовала casting.
+    public static void main(String[] args) {
+ char bookva = 2;
+ int chislo = 1;
+ int sumbookva = (int)(bookva + chislo);
+ System.out.println(sumbookva);
+
+    }
+}
