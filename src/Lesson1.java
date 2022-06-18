f201a8dfae037e33a90d228473bf5a521a90789b
Olelife13 <olelife13@gmail.com>
2022-06-18 20:29:53 +0700

    Задание 1

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -1,2 +1,20 @@
 public class Lesson1 {
+    public static void main(String[] args) {
+        int balance = 100;
+        int TotalBalance = increaseBalance (balance);
+
+        System.out.println(TotalBalance);
+    }
+
+    public static int increaseBalance(int balance) {
+        int result;
+        int bonus = 13;
+        result = balance + bonus;
+        return result;
+    }
+
+
+    // Пойдет не так вот что, при цикле или больших числах будет превышено максимальное положительное значение int,
+    // и баланс станет отрицательным.
+
 }
