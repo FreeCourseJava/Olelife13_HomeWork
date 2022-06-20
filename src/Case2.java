386546daca031538f649d81f5f942a103d914dd3
Olelife13 <olelife13@gmail.com>
2022-06-21 00:07:01 +0700

    Задание 1 внесла правки по комментам.

--- src/Case2.java
+++ src/Case2.java
@@ -1,21 +1,23 @@
 public class Case2 {
     public static void main(String[] args) {
+
         int balance = 2147483645;
-        int TotalBalance = increaseBalance(balance);
+        int bonus = 13;
+
+
+        int TotalBalance = increaseBalance(balance,bonus);
 
         System.out.println(TotalBalance);
     }
 
-    public static int increaseBalance(int balance) {
+    public static int increaseBalance(int balance,int bonus) {
         int result;
-        int bonus = 13;
-        int summMax = 2147483647;
-        int summMin = -2147483648;
         int intFlow = 0;
 
         result = balance + bonus;
 
         if (result < intFlow) {
+
             System.out.println(intFlow);
         } else {
             System.out.println(result);
