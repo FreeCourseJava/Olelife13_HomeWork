b3832c322687a85942ad196b959d19801b56d39d
Olelife13 <olelife13@gmail.com>
2022-06-21 00:39:16 +0700

    Задание 2 поправила кастинг

--- src/Case2.java
+++ src/Case2.java
@@ -1,11 +1,9 @@
 public class Case2 {
-    // ПервоначальнояЯ видела только грустную надпись "java: incompatible types: possible lossy conversion
-    // from int to char" и грустила про несовместимость всего живого в этом мире. После второго урока немного
-    // попробовала casting.
+
     public static void main(String[] args) {
  char bookva = 2;
  int chislo = 1;
- int sumBookva = (int)(bookva + chislo);
+        char sumBookva = (char)(bookva + chislo);
  System.out.println(sumBookva);
 
     }
