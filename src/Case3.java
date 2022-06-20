986152cf974b54fbfadf197c0f748924bc92348e
Olelife13 <olelife13@gmail.com>
2022-06-21 00:12:38 +0700

    Задание 3

--- src/Case3.java
+++ src/Case3.java
@@ -1,29 +1,30 @@
 public class Case3 {
     public static void main(String[] args) {
 
-        int balance = 1000;
+        int balance = 21474847;
+        int bonus = -3000;
 
-        int TotalBalance = increaseBalance(balance);
-        System.out.println(TotalBalance);
+        long totalBalance = increaseBalance(balance,bonus);}
 
-    }
-        public static int increaseBalance(int balance) {
-            int result;
-            int bonus = 13;
-            int summMax = 2147483640;
-            int summMin = -2147483640;
-            int intFlow = 0;
+    public static int increaseBalance(int balance, int bonus) {
+        int result;
+        int intFlow = 0;
 
-            result = balance+bonus;
+        long summ;
+        long balanceL;
+        long bonusL;
+        balanceL = (long) balance;
+        bonusL = (long) bonus;
 
-            int indicator = balance*bonus;
-            System.out.println(indicator);
-            if (indicator >0) {
-                if (result > summMax){
-                System.out.println(intFlow);
-            } else if (result < summMin){
-                System.out.println(intFlow);
-            } else System.out.println(result);}
+
+          summ = balanceL+bonusL;
+                if (summ>Integer.MAX_VALUE|summ<Integer.MIN_VALUE){
+                    System.out.println("выход за рамки возможностей счета: " + intFlow);
+                }
+                   else System.out.println("Сумма на счете: " + summ);
+
+          result=(int) summ;
             return result;
-        }
+            }
+
 }
