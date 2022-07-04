666439eef446f14a4c6a61d2d57fa071b0f33a13
Olelife13 <olelife13@gmail.com>
2022-07-04 23:47:29 +0700

    исправляю конфликт

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -1,20 +1,3 @@
-public class Lesson1 {
-    public static void main(String[] args) {
-        int balance = 100;
-        int TotalBalance = increaseBalance (balance);
+public class Lesson1{
 
-        System.out.println(TotalBalance);
-    }
-
-    public static int increaseBalance(int balance) {
-        int result;
-        int bonus = 13;
-        result = balance + bonus;
-        return result;
-    }
-
-
-    // Пойдет не так вот что, при цикле или больших числах будет превышено максимальное положительное значение int,
-    // и баланс станет отрицательным.
-
-}
+  }
